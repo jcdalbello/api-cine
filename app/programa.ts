e47a3677107e8bfc79f30bfe6aec96524d14aad9
Juan Cruz Dal Bello <juancruzdalbello17@gmail.com>
2025-12-23 03:32:53 -0300

    primera version del endpoint GET /peliculas

--- app/programa.ts
+++ app/programa.ts
@@ -3,10 +3,14 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const puerto: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res
     .status(200)
     .send("El servidor esta funcionando");
+});
+
+app.get("/peliculas", (req: Request, res: Response) => {
+  res.status(200).send();
 });
 
 const server = app
--- test/programa.test.ts
+++ test/programa.test.ts
@@ -16,3 +16,12 @@ describe("GET /", () => {
     expect(respuesta.status).toEqual(CODIGO_OPERACION_EXITOSA);
   });
 });
+
+describe("GET /peliculas", () => {
+  const urlPeliculas: string = "/peliculas";
+  
+  test("deberia devolver un codigo 200", async () => {
+    const respuesta = await requestWithSupertest.get(urlPeliculas);
+    expect(respuesta.status).toEqual(CODIGO_OPERACION_EXITOSA);
+  });
+});
