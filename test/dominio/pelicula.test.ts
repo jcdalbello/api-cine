e6ce9c20fdbe50c680a1e75468954af18d2a60ef
Juan Cruz Dal Bello <juancruzdalbello17@gmail.com>
2025-12-23 12:44:04 -0300

    refactorizacion de los tests de Pelicula

--- test/dominio/pelicula.test.ts
+++ test/dominio/pelicula.test.ts
@@ -2,40 +2,45 @@ import Pelicula from "../../app/dominio/pelicula";
 import CampoIncorrectoPeliculaError from "../../app/errores/campo-incorrecto-pelicula-error";
 
 describe("Pelicula", () => {
+  const id: string = "1";
+  const titulo: string = "pelicula1";
+  const genero: string = "genero1";
+  const longitudMaximaCaracteres: number = 70;
+  const tituloDemasiadoLargo: string = "a".repeat(longitudMaximaCaracteres + 1);
+  const generoDemasiadoLargo: string = "a".repeat(longitudMaximaCaracteres + 1);
+  
+
   test("deberia crear un objeto Pelicula", () => {
-    const pelicula: Pelicula = new Pelicula("1", "pelicula1", "genero1");
+    const pelicula: Pelicula = new Pelicula(id, titulo, genero);
     expect(pelicula).toBeInstanceOf(Pelicula);
   });
 
   test("deberia crear una pelicula con los datos correctos", () => {
-    const pelicula: Pelicula = new Pelicula("1", "pelicula1", "genero1");
-    expect(pelicula.obtenerId()).toEqual("1");
-    expect(pelicula.obtenerTitulo()).toEqual("pelicula1");
-    expect(pelicula.obtenerGenero()).toEqual("genero1");
+    const pelicula: Pelicula = new Pelicula(id, titulo, genero);
+    expect(pelicula.obtenerId()).toEqual(id);
+    expect(pelicula.obtenerTitulo()).toEqual(titulo);
+    expect(pelicula.obtenerGenero()).toEqual(genero);
   });
 
   test("deberia devolver un error CampoIncorrectoPeliculaError al intentar crear una pelicula con un id vacio", () => {
-    expect(() => new Pelicula("", "pelicula1", "genero1")).toThrow(CampoIncorrectoPeliculaError);
+    expect(() => new Pelicula("", titulo, genero)).toThrow(CampoIncorrectoPeliculaError);
   });
 
   test("deberia devolver un error CampoIncorrectoPeliculaError al intentar crear una pelicula con un titulo vacio", () => {
-    expect(() => new Pelicula("1", "", "genero1")).toThrow(CampoIncorrectoPeliculaError);
+    expect(() => new Pelicula(id, "", genero)).toThrow(CampoIncorrectoPeliculaError);
   });
 
   test("deberia devolver un error CampoIncorrectoPeliculaError al intentar crear una pelicula con un titulo mas largo de lo permitido", () => {
-    const longitudMaximaCaracteres: number = 70;
     const tituloDemasiadoLargo: string = "a".repeat(longitudMaximaCaracteres + 1);
-    expect(() => new Pelicula("1", tituloDemasiadoLargo, "genero1")).toThrow(CampoIncorrectoPeliculaError);
+    expect(() => new Pelicula(id, tituloDemasiadoLargo, genero)).toThrow(CampoIncorrectoPeliculaError);
   });
 
   test("deberia devolver un error CampoIncorrectoPeliculaError al intentar crear una pelicula con un genero vacio", () => {
-    expect(() => new Pelicula("1", "pelicula1", "")).toThrow(CampoIncorrectoPeliculaError);
+    expect(() => new Pelicula(id, titulo, "")).toThrow(CampoIncorrectoPeliculaError);
   });
 
   test("deberia devolver un error CampoIncorrectoPeliculaError al intentar crear una pelicula con un genero mas largo de lo permitido", () => {
-    const longitudMaximaCaracteres: number = 70;
-    const generoDemasiadoLargo: string = "a".repeat(longitudMaximaCaracteres + 1);
-    expect(() => new Pelicula("1", "pelicula1", generoDemasiadoLargo)).toThrow(CampoIncorrectoPeliculaError);
+    expect(() => new Pelicula(id, titulo, generoDemasiadoLargo)).toThrow(CampoIncorrectoPeliculaError);
   });
 
   test("deberia devolver todos los mensajes de error al intentar crear una pelicula con todos los datos vacios", () => {
@@ -51,12 +56,8 @@ describe("Pelicula", () => {
   });
   
   test("deberia devolver todos los mensajes de error al intentar crear una pelicula con todos los datos vacios", () => {
-    const longitudMaximaCaracteres: number = 70;
-    const tituloDemasiadoLargo: string = "a".repeat(longitudMaximaCaracteres + 1);
-    const generoDemasiadoLargo: string = "a".repeat(longitudMaximaCaracteres + 1);
-
     try {
-      new Pelicula("1", tituloDemasiadoLargo, generoDemasiadoLargo);
+      new Pelicula(id, tituloDemasiadoLargo, generoDemasiadoLargo);
     } catch (error) {
       if (error instanceof CampoIncorrectoPeliculaError) {
         expect(error.titulo).toEqual("El titulo no puede superar el limite de caracteres");
