d1f928bf2f6a9cf82bbef4f649aeeee1b0fb0cb6
Juan Cruz Dal Bello <juancruzdalbello17@gmail.com>
2025-12-29 23:05:25 -0300

    se valida que el repositorio de salas postgresql devuelve los datos correctos al guardar mas de una sala

--- test/adaptadores/repositorio-sala-postgresql.test.ts
+++ test/adaptadores/repositorio-sala-postgresql.test.ts
@@ -4,14 +4,35 @@ import Sala from "../../app/dominio/sala";
 describe("RepositorioSalaPostgreSQL", () => {
   const repositorioSalaPostgreSQL: RepositorioSalaPostgreSQL = new RepositorioSalaPostgreSQL();
 
-  test("deberia crear un objeto RepositorioSalaPostgreSQL", () => {
-    expect(repositorioSalaPostgreSQL).toBeInstanceOf(RepositorioSalaPostgreSQL);
-  });
+  describe("guardar", () => {
+    test("deberia crear un objeto RepositorioSalaPostgreSQL", () => {
+      expect(repositorioSalaPostgreSQL).toBeInstanceOf(RepositorioSalaPostgreSQL);
+    });
 
-  test("deberia guardar una sala y devolver el resultado", () => {
-    const sala: Sala = new Sala(0, 50);
-    const salaGuardada: Sala = repositorioSalaPostgreSQL.guardar(sala);
-    expect(salaGuardada.obtenerId()).toEqual(1);
-    expect(salaGuardada.obtenerCapacidad()).toEqual(sala.obtenerCapacidad());
+    test("deberia guardar una sala y devolver el resultado", () => {
+      const sala: Sala = new Sala(0, 50);
+      const salaGuardada: Sala = repositorioSalaPostgreSQL.guardar(sala);
+      expect(salaGuardada.obtenerId()).toEqual(1);
+      expect(salaGuardada.obtenerCapacidad()).toEqual(sala.obtenerCapacidad());
+    });
+
+    test("deberia guardar una sala y devolver el resultado", () => {
+      const sala: Sala = new Sala(0, 50);
+      const salaGuardada: Sala = repositorioSalaPostgreSQL.guardar(sala);
+      expect(salaGuardada.obtenerId()).toEqual(1);
+      expect(salaGuardada.obtenerCapacidad()).toEqual(sala.obtenerCapacidad());
+    });
+
+    test("deberia guardar multiples salas en la base de datos y devolver los datos correctos", () => {
+      const sala: Sala = new Sala(0, 50);
+      const salaGuardada: Sala = repositorioSalaPostgreSQL.guardar(sala);
+      expect(salaGuardada.obtenerId()).toEqual(1);
+      expect(salaGuardada.obtenerCapacidad()).toEqual(sala.obtenerCapacidad());
+
+      const sala2: Sala = new Sala(0, 150);
+      const salaGuardada2: Sala = repositorioSalaPostgreSQL.guardar(sala2);
+      expect(salaGuardada2.obtenerId()).toEqual(2);
+      expect(salaGuardada2.obtenerCapacidad()).toEqual(sala2.obtenerCapacidad());
+    });
   });
 });
