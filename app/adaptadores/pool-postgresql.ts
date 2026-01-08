b28556f1ee1a450c05b9dfb8e9e26515d176c431
Juan Cruz Dal Bello <juancruzdalbello17@gmail.com>
2026-01-08 13:14:55 -0300

    el pool de la db postgres usa variables de entorno en vez de valores hardcodeados

--- app/adaptadores/pool-postgresql.ts
+++ app/adaptadores/pool-postgresql.ts
@@ -1,9 +1,10 @@
 import { Pool } from "pg";
+import "dotenv/config";
 
 export const pool = new Pool({
-  user: 'usuario',
-  host: 'localhost',
-  database: 'api_cine_db',
-  password: 'password',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
